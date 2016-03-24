297dc4bc5e21322fbbab2fee0e39a2b3d5c223e2
qianfeng <qianfeng@hanhailong.local>
2016-03-24 15:12:05 +0800

    初始化项目

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright (c) 2016年 LJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
